fcb743e6c42f642585889bee24a4982a55818de8
firzatullahd <firzatullahdwikoramadhan@gmail.com>
2021-04-22 12:37:43 +0700

    remove scss, and add emotion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { AiFillHome } from "react-icons/ai";
-import { SiPokemon } from "react-icons/si";
-import { CgPokemon } from "react-icons/cg";
+import styled from "@emotion/styled";
+import { useLocation } from "react-router-dom";
+import pokemonLogo from "../img/pokemon-logo.png";
+import pokeball from "../img/pokeball.png";
 
 export default function Navbar() {
+  const location = useLocation();
+  const url = location.pathname;
   return (
-    <nav>
-      <Link to="/">
-        <SiPokemon size="2rem" />
-      </Link>
-      <Link to="/my-pokemon">
-        <CgPokemon size="2rem" />
-      </Link>
-    </nav>
+    <StyledNav>
+      {url === "/pokedex" ? (
+        ""
+      ) : (
+        <Link to="/pokedex">
+          <button>
+            <img src={pokemonLogo} alt="pokedex" className="pokedex" />
+            Pokédex
+          </button>
+        </Link>
+      )}
+
+      {url === "/my-pokemon" ? (
+        ""
+      ) : (
+        <Link to="/my-pokemon">
+          <button>
+            <img src={pokeball} alt="pokeball" />
+            Pokemon
+          </button>
+        </Link>
+      )}
+    </StyledNav>
   );
 }
+
+const StyledNav = styled.nav`
+  background: #ffffff;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 1rem 1rem 0 0;
+  box-shadow: 0px -15px 30px -5px rgba(0, 0, 0, 0.2);
+  button {
+    border: none;
+    padding: 0.3rem;
+    background: transparent;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 0.5rem;
+  }
+  img {
+    width: 30px;
+    height: 30px;
+  }
+  .pokedex {
+    width: 90px;
+  }
+`;
